432256f5e38615d1ef416672f4cc75c53107cb4c
arziiann <127401577+arziiann@users.noreply.github.com>
2023-05-10 18:41:57 +0400

    Add files via upload

---
+++ home_work.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <tuple>
+
+
+int main()
+{
+	/*std::tuple<int, char, bool, float> array(45, 'A', false, 78.76);
+	std::cout << std::get<0>(array) << std::endl;*/
+
+	const int array_size = 4;
+	
+
+	int int_value = 10;
+	float  float_value = 89.7;
+	char  char_value = 'A';
+	bool  bool_value = false;
+
+	void* array[array_size] = { &int_value, &float_value, &char_value, &bool_value };
+	
+
+	
+	int * int_ptr = static_cast<int*>(array[0]);
+	float * float_ptr = static_cast<float*>(array[1]);
+	char * char_ptr = static_cast<char*>(array[2]);
+	bool * bool_ptr = static_cast<bool*>(array[3]);
+	
+	std::cout << *int_ptr  <<  std::endl;
+	std::cout << *float_ptr << std::endl;
+	std::cout << *char_ptr << std::endl;
+	std::cout << *bool_ptr << std::endl;
+
+}
