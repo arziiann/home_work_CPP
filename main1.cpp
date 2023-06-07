4eccb09aa56dd5da1b7390208e1c552d4dd71206
arziiann <127401577+arziiann@users.noreply.github.com>
2023-06-07 17:58:50 +0400

    Add files via upload

--- main1.cpp
+++ main1.cpp
@@ -1,36 +1,53 @@
 #include <iostream>
+#include <string>
 
-int num_cout_sum(int number);
-
+char * strcat_function(char* s1, char* s2);
 
 int main()
 {
-	int num = 0;
-	std::cout << "Enter number" << std::endl;
-	std::cin >> num;
-	if(num < 0)
+	int size1 = 0;
+	std::cout << "Enter 1 line size: ";
+	std::cin >> size1;
+
+	char * line1 = new char[size1]; 
+	for(int i = 0; i < size1; ++i)
 	{
-		num = num * (-1);
-		std::cout << num_cout_sum(num);
+		std::cin >> line1[i];
 	}
-	else
-	{		
-		std::cout << num_cout_sum(num);
+
+	int size2 = 0;
+	std::cout << "Enter 2 line size: ";
+	std::cin >> size2;
+	char * line2 = new char[size2];
+	for(int i = 0; i < size2; ++i)
+	{
+		std::cin >> line2[i];
 	}
 	
-	return 0;
+	std::cout << strcat_function(line1, line2) << std::endl;
+
 }
 
-int num_cout_sum(int number)
+
+
+char * strcat_function(char* s1, char* s2)
 {
 	int count = 0;
-	while(number != 0)
+	char * line3 = new char[strlen(s1) + strlen(s2)];
+	for(int i = 0; i < strlen(s1); ++i, ++count)
 	{
-		count += number % 10;
-		number /= 10;
-
+		line3[i] = s1[i];
 	}
+	
+	for(int i = 0; i < strlen(line3); ++i, ++count)
+	{
+		line3[count] = s2[i];
+		
+	}	
+	delete[] s1;
+	s1 = line3;
+	line3 = nullptr;
+	return s1;
 
-	return count;
 
 }
